ff122f382aedbd00106025c8c8f368237b29c79d
Jimin Hong <84127399+jm3789@users.noreply.github.com>
2025-03-11 19:03:12 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 5008 ms, Memory: 188080 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -0,0 +1,58 @@
+# heapq는 특정 값을 직접 삭제하는 기능이 없음  
+# 특정 값을 삭제하려면 list.remove() 후 heapify()가 필요 → O(N)으로 비효율적  
+# --> valid 딕셔너리를 사용하여 삭제 여부(해당 숫자가 현재 힙에 몇 개 있는지)를 동기화
+
+import sys
+import heapq
+
+T = int(input())
+for _ in range(T):
+    k = int(input())
+    min_heap = []
+    max_heap = []
+    valid = {}
+    for _ in range(k):
+        q, num = sys.stdin.readline().split()
+        num = int(num)
+        if q == 'I':  # 삽입
+            heapq.heappush(min_heap, num)
+            heapq.heappush(max_heap, -1*num)
+            valid[num] = valid.get(num, 0) + 1
+        elif q == 'D':  # 삭제
+            if num == 1:  # 최대값 삭제
+                while max_heap:
+                    d = -1*heapq.heappop(max_heap)
+                    if valid[d] > 0:
+                        valid[d] -= 1
+                        break
+            else:  # 최소값 삭제
+                while min_heap:
+                    d = heapq.heappop(min_heap)
+                    if valid[d] > 0:
+                        valid[d] -= 1
+                        break
+
+    # valid에 0인 숫자가 힙에 남아있다면 제거
+    while max_heap:
+        d = -1*max_heap[0]
+        if valid[d] == 0:
+            heapq.heappop(max_heap)
+        else:
+            break
+    while min_heap:
+        d = min_heap[0]
+        if valid[d] == 0:
+            heapq.heappop(min_heap)
+        else:
+            break
+
+    empty_flag = True
+    for k, v in valid.items():
+        if v != 0:
+            empty_flag = False
+            break
+    if empty_flag:
+        print('EMPTY')
+    else:
+        print(-1*max_heap[0], min_heap[0])
+        
