945144bd09fe3ca3b0b72cb32fa3daf626055357
Jimin Hong <84127399+jm3789@users.noreply.github.com>
2025-03-19 01:06:35 +0900

    [Gold V] Title: 평범한 배낭, Time: 3556 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -6,7 +6,7 @@ for i in range(1, N+1):
     W[i], V[i] = map(int, input().split())
 
 # dp[i][j]: i번째 물건까지 고려하고, j무게까지 담을 수 있을 때 최대 가치
-dp = [[0 for _ in range(K+1)] for _ in range(N+1)]  
+# dp = [[0 for _ in range(K+1)] for _ in range(N+1)]  
 
 # w v
 #       0kg 1kg 2kg 3kg 4kg 5kg 6kg 7kg
@@ -16,13 +16,18 @@ dp = [[0 for _ in range(K+1)] for _ in range(N+1)]
 # 3 6    0   0   0   6   8   8   13  14
 # 5 12   0   0   0   6   8   12  13  14
 
+# 점화식: max(dp[i-1][j] , dp[i-1][j-W[i]] + V[i]])
+# --> 1차원 dp배열로 최적화 가능: 바로 이전 행(i - 1)을 제외한 그 이전 행들의 정보(1 ~ i - 2)들은 활용되지 않기 때문
+# 점화식: max(dp[j] , dp[j-W[i]] + V[i]])
+# 이때  dp[j-W[i]]이 영향을 받지 않으려면 "뒤에서부터 갱신"해야 함
+
+dp = [0 for _ in range(K+1)]
+
 for i in range(1, N+1):
-    for j in range(1, K+1):
-        # 점화식: max(dp[i-1][j] , dp[i-1][j-W[i]] + V[i]])
+    for j in range(K, 0, -1):
+        
         if j >= W[i]:
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-W[i]] + V[i])
-        else:
-            dp[i][j] = dp[i-1][j]
+            dp[j] = max(dp[j], dp[j-W[i]] + V[i])
 
 # print(dp)
-print(dp[N][K])
+print(dp[K])
