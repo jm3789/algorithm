8792f39e0b908fd8ab549886e1d5a26d0a48b9b6
Jimin Hong <84127399+jm3789@users.noreply.github.com>
2025-03-19 17:02:50 +0900

    [Gold III] Title: 앱, Time: 388 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.py
+++ 백준/Gold/7579. 앱/앱.py
@@ -1,21 +1,25 @@
 # 세로로 앱 정보: (바이트, 비용)
-# 가로로 바이트 수
+# 가로로 비용
 
 # i : 앱 번호
-# dp[j] = j비트를 확보했을 때의 최소 비용
+# dp[j] = j비용으로 확보할 수 있는 최대 바이트
 N, M = map(int, input().split())
 A = [0] + list(map(int, input().split()))  # 바이트 수
 C = [0] + list(map(int, input().split()))  # 비용
-dp = [sum(C) + 1 for _ in range(M+1)]
+
+dp = [0 for _ in range(sum(C) + 1)]
 
 # 점화식
-# dp[j] = min(dp[j], dp[j - A[i]] + C[i])
+# dp[j] = max(dp[j], dp[j - C[i]] + A[i])
 for i in range(1, N+1):
-    for j in range(M, -1, -1): # 뒤에서부터 갱신
-        if A[i] >= j:
-            # A[i] : i번째 앱의 바이트 수가 현재 확보해야 하는 바이트 수보다 크거나 같다면
-            dp[j] = min(dp[j], C[i])
-        else:
-            dp[j] = min(dp[j], dp[j - A[i]] + C[i])
+    for j in range(sum(C), -1, -1): # 뒤에서부터 갱신
+        # 이 앱의 비용이 j비용보다 작거나 같을 때에만 이 앱을 고려할 수 있음
+        if j >= C[i]:
+            dp[j] = max(dp[j], dp[j - C[i]] + A[i])
     # print(dp)
-print(dp[M])
+
+# 처음으로 M바이트 이상 확보할 수 있는 지점을 출력
+for i in range(sum(C) + 1):
+    if dp[i] >= M:
+        print(i)
+        break
