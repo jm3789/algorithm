166b39308f6077b6c8df77d0fde26e3e51a93291
Jimin Hong <84127399+jm3789@users.noreply.github.com>
2025-05-14 03:20:08 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 368 ms, Memory: 52716 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,16 +1,17 @@
 # 트리 상에서 연결: 부모가 하나
 # DFS
 
+import sys
 from collections import deque
 
-N = int(input())
+N = int(sys.stdin.readline())
 node_parents = [0] * (N + 1)  # 0번째 인덱스는 사용하지 않음, 1번째 인덱스가 루트 노드
 node_parents[0] = -1  # 사용하지 않는 인덱스
 node_parents[1] = -1  # 루트 노드의 부모는 -1로 설정
 node_map = [ [] for _ in range(N + 1)]  # 0번째 인덱스는 사용하지 않음, 1번째 인덱스가 루트 노드
 
 for _ in range(N-1):
-    a, b = map(int, input().split())
+    a, b = map(int, sys.stdin.readline().split())
     node_map[a].append(b)
     node_map[b].append(a)
 
