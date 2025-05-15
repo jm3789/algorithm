25bf55b8fddc6da80556bf3becda3776a310a6bd
Jimin Hong <84127399+jm3789@users.noreply.github.com>
2025-05-15 22:38:00 +0900

    [Silver I] Title: 트리 순회, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -1,23 +1,15 @@
-def dfs_prefix(node):
-    if node == '.':
-        return
-    print(node, end='')
-    dfs_prefix(node_dict[node][0])
-    dfs_prefix(node_dict[node][1])
+prefix = []
+infix = []
+postfix = []
 
-def dfs_infix(node):
+def dfs(node):
     if node == '.':
         return
-    dfs_infix(node_dict[node][0])
-    print(node, end='')
-    dfs_infix(node_dict[node][1])
-
-def dfs_postfix(node):
-    if node == '.':
-        return
-    dfs_postfix(node_dict[node][0])
-    dfs_postfix(node_dict[node][1])
-    print(node, end='')
+    prefix.append(node)
+    dfs(node_dict[node][0])
+    infix.append(node)
+    dfs(node_dict[node][1])
+    postfix.append(node)
 
 N = int(input())
 node_dict = {}
@@ -25,9 +17,7 @@ for _ in range(N):
     a, b, c = input().split()
     node_dict[a] = [b, c]
 
-dfs_prefix('A')
-print()
-dfs_infix('A')
-print()
-dfs_postfix('A')
-print()
+dfs('A')
+print(''.join(prefix))
+print(''.join(infix))
+print(''.join(postfix))
