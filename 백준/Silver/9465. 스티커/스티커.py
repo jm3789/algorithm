9c075162cd2e19965ddff47e2253b58b7a5757c7
Jimin Hong <84127399+jm3789@users.noreply.github.com>
2025-05-18 17:29:53 +0900

    [Silver I] Title: 스티커, Time: 724 ms, Memory: 49528 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -6,19 +6,19 @@ for _ in range(T):
     n = int(input())
 
     # 스티커 정보 입력받기
-    a = list(map(int, input().split()))
-    b = list(map(int, input().split()))
-    stickers = [[0, 0]] + [[a[i], b[i]] for i in range(n)]
+    a = [0] + list(map(int, input().split()))
+    b = [0] + list(map(int, input().split()))
+    stickers = [a, b]
 
     # dp 리스트: 각 단계에서 해당 스티커로 끝났을 때 얻을 수 있는 최대 점수
-    dp = [[0, 0] for _ in range(n+1)]
-    dp[1][0] = stickers[1][0]
+    dp = [[0 for _ in range(n+1)] for _ in range(2)]
+    dp[0][1] = stickers[0][1]
     dp[1][1] = stickers[1][1]
 
     # dp 점화식
     for i in range(2, n+1):
-        dp[i][0] = stickers[i][0] + max(dp[i-2][1], dp[i-1][1])
-        dp[i][1] = stickers[i][1] + max(dp[i-2][0], dp[i-1][0])
+        dp[0][i] = max(dp[1][i-2], dp[1][i-1]) + stickers[0][i]
+        dp[1][i] = max(dp[0][i-2], dp[0][i-1]) + stickers[1][i]
 
     # 결과 출력
-    print(max(dp[n][0], dp[n][1]))
+    print(max(dp[0][n], dp[1][n]))
