ac8be5cd6d79166f2c6495ff7ae793fe4be8f813
Jimin Hong <84127399+jm3789@users.noreply.github.com>
2025-05-25 01:06:14 +0900

    [Silver I] Title: 곱셈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,19 +1,2 @@
-# 분할정복
-# 나머지 분배 법칙
-# (A*B) % C = (A%C) * (B%C) % C
-
 A, B, C = map(int, input().split())
-
-def solution(A, B, C):
-    if B == 1:
-        return A % C
-
-    # 두 개로 분할
-    divided = solution(A, B//2, C)
-
-    if B % 2 == 0:  # B가 짝수
-        return divided * divided % C
-    else:  # B가 홀수
-        return divided * divided * (A % C) % C
-
-print(solution(A, B, C))
+print(pow(A, B, C))
